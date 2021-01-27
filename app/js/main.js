43725b5bd58f341d4e49599bf4673050dcea801c
Gelena Dmitrieva <dmitrievaGelenaOneTwo@gmail.com>
2021-01-28 00:19:39 +0300

    add header blog

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
-$(function() {
-
+$(function () {
+    $('.box-slider__content').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: ' <button class="slick-arrow prev-arrow"><img src="img/header/prev.png" alt=""></button>',
+        nextArrow: ' <button class="slick-arrow next-arrow"><img src="img/header/next.png" alt=""></button>',
+      });
 });
+
+//добавить обработчики на ссылки в header
