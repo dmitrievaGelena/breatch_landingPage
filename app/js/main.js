0221fe37c690f414212091f70ccf23dc770bfb1b
Gelena Dmitrieva <dmitrievaGelenaOneTwo@gmail.com>
2021-02-05 21:43:19 +0300

    add section product

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,7 @@ $(function () {
         prevArrow: ' <button class="slick-arrow prev-arrow"><img src="img/header/prev.png" alt=""></button>',
         nextArrow: ' <button class="slick-arrow next-arrow"><img src="img/header/next.png" alt=""></button>',
       });
+      
 });
 
 //добавить обработчики на ссылки в header
